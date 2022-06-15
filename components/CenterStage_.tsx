6e0b83abd87627b48199d9ac3ee7d012113b5425
Lwazi Ndlovu <42185877+lwazin@users.noreply.github.com>
2022-06-15 18:32:31 +0200

    user interface :: CenterStage_.tsx, atoms.ts :: 'Had trouble recreating Flutters colorFilter. Found a solution around it, more of a manual solution!'

--- components/CenterStage_.tsx
+++ components/CenterStage_.tsx
@@ -1,27 +1,62 @@
+import { useRecoilState } from 'recoil'
+import { colorState } from '../components/atoms/atoms'
+
 interface CenterStage_Props {}
 
 const CenterStage_ = ({}: CenterStage_Props) => {
+  const [colors_, setColors_] = useRecoilState(colorState)
   return (
     <div
       className={`rounded-[4px] shadow-md bg-white w-[830px] h-[600px] absolute top-[70px] left-0 overflow-hidden`}
     >
       <div className={`w-full h-[35px] bg-green-300`} />
       <div
-        className={`w-full h-[400px] mt-[40px] flex flex-row items-center justify-center relative`}
+        className={`w-full h-[400px] flex flex-row items-center justify-center relative mb-4 mt-4`}
       >
         <img
           src={`/assets/images/hoodie.png`}
           className={`h-full object-cover ml-[100px]`}
+          style={{filter: colors_['pink']}}
         />
-        <div className={`absolute h-[130px] w-[150px] mt-1 opacity-90 flex items-center justify-center left-[175px] top-[90px]`}>
+        <img
+          src={`/assets/images/hoodie.png`}
+          className={`h-full object-cover absolute left-[100px]`}
+          style={{mixBlendMode: 'multiply'}}
+        />
+        <div
+          className={`absolute h-[130px] w-[150px] mt-1 opacity-100 flex items-center justify-center left-[175px] top-[90px]`}
+        >
           <img
             src={`/assets/images/LwaziNF.png`}
             className={`h-full object-cover`}
+            style={{filter: colors_['blue']}}
           />
         </div>
         <div
           className={`w-[350px] h-[400px] ml-auto mr-[30px] bg-gray-50 border-solid border-gray-300 border-[0.5px] rounded-[4px] mb-1`}
         ></div>
+      </div>
+      <div
+        className={`w-full h-[133px] flex flex-row items-center justify-center py-4`}
+      >
+        <div className={`h-full p-4 bg-gray-50 border-solid border-gray-200 border-[1px] flex items-center justify-center rounded-[4px] mx-1 cursor-pointer hover:bg-gray-100 transition-all duration-200`}>
+          <img
+            src={`/assets/images/hoodie.png`}
+            className={`h-full object-cover`}
+          />
+        </div>
+        {/* <div className={`h-full p-2`}>
+          <img
+            src={`/assets/images/shoe.png`}
+            className={`h-full object-cover`}
+          />
+        </div> */}
+        <div className={`h-full p-4 bg-gray-50 border-solid border-gray-200 border-[1px] flex items-center justify-center rounded-[4px] mx-1 cursor-pointer hover:bg-gray-100 transition-all duration-200`}>
+          <img
+            src={`/assets/images/cap.png`}
+            className={`h-full object-cover`}
+          />
+        </div>
       </div>
     </div>
   );
--- components/atoms/atoms.ts
+++ components/atoms/atoms.ts
@@ -1,6 +1,26 @@
 import { atom } from "recoil";
 
-export const wallet_ = atom({
-    key: 'default',
-    default: false
-})
+export const walletState = atom({
+  key: "walletState",
+  default: false,
+});
+
+export const sizeState = atom({
+    key: "sizeState",
+    default: 'medium',
+  });
+
+export const colorState = atom({
+  key: "colorState",
+  default: {
+    white: "invert(0%) sepia(4%) saturate(0%) hue-rotate(309deg) brightness(100%) contrast(107%)",
+    brown: "invert(15%) sepia(95%) saturate(1549%) hue-rotate(19deg) brightness(96%) contrast(101%)",
+    black: "invert(100%) sepia(0%) saturate(7484%) hue-rotate(238deg) brightness(110%) contrast(103%)",
+    green: "invert(20%) sepia(37%) saturate(1578%) hue-rotate(81deg) brightness(94%) contrast(105%)",
+    yellow: "invert(87%) sepia(39%) saturate(4180%) hue-rotate(356deg) brightness(100%) contrast(102%)",
+    blue: "invert(11%) sepia(98%) saturate(5807%) hue-rotate(252deg) brightness(88%) contrast(140%)",
+    lightblue: "invert(71%) sepia(38%) saturate(7500%) hue-rotate(163deg) brightness(101%) contrast(101%)",
+    pink: "invert(34%) sepia(100%) saturate(6331%) hue-rotate(307deg) brightness(97%) contrast(117%)",
+    purple: "invert(8%) sepia(96%) saturate(5946%) hue-rotate(273deg) brightness(43%) contrast(106%)"
+  },
+});
