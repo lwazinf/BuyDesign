2ba14edd4bbef1f0284a4ddaf475914add2cf274
Lwazi Ndlovu <42185877+lwazin@users.noreply.github.com>
2022-06-17 15:51:39 +0200

    Documentation :: BuyDesign.sol :: 'Done with Documentation'

--- contracts/BuyDesign.sol
+++ contracts/BuyDesign.sol
@@ -12,12 +12,15 @@ contract BuyDesign {
         owner = msg.sender;
     }
 
+    // initOrder takes in an object for of design guidelines
     function initOrder(string memory details) public payable {
         orderDetails = details;
+        // Had trouble with setting ethereum payable amount..
         // require(msg.value >= 25959628, "Please send the required ETH amount");
         console.log('Order made, thank you for your interest in our service!');
     }
 
+    // viewOrder allows user to view guidelines
     function viewOrder() public view returns (string memory) {
         console.log(orderDetails);
         return orderDetails;
